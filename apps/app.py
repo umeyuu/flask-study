bf47ff999b47d20550fd6e17e6b45d7282d718b5
umeyuu <umeyu7777777@gmail.com>
2022-12-30 17:02:47 +0900

    chapter2.6

--- apps/app.py
+++ apps/app.py
@@ -3,9 +3,12 @@ from pathlib import Path
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 # SQLAlchemyをインスタンス化する
 db = SQLAlchemy()
+
+csrf = CSRFProtect()
 
 
 def create_app():
@@ -16,7 +19,10 @@ def create_app():
         SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
         SQLALCHEMY=True,
+        WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f",
     )
+
+    csrf.init_app(app)
 
     # SQLAlchemyとアプリを連携する
     db.init_app(app)
--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
 
 from apps.app import db
+from apps.crud.forms import UserForm
 from apps.crud.models import User
 
 # Blueprintでアプリを生成する
@@ -16,3 +17,54 @@ def index():
 def sql():
     db.session.query(User).all()
     return "コンソールログを確認して下さい"
+
+
+@crud.route("/users/new", methods=["GET", "POST"])
+def create_user():
+    form = UserForm()
+
+    if form.validate_on_submit():
+        # ユーザーを作成する
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        # ユーザーを追加してコミットする
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("crud.users"))
+    return render_template("crud/create.html", form=form)
+
+
+@crud.route("/users")
+def users():
+    users = User.query.all()
+    return render_template("crud/index.html", users=users)
+
+
+@crud.route("/users/<user_id>", methods=["GET", "POST"])
+def edit_user(user_id):
+    form = UserForm()
+
+    user = User.query.filter_by(id=user_id).first()
+
+    # formからサブミットされた場合はユーザーを更新しユーザーの一覧画面へリダイレクトする
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("crud.users"))
+    # GETの場合はHTMLを返す
+    return render_template("crud/edit.html", user=user, form=form)
+
+
+@crud.route("/users/<user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for("crud.users"))
