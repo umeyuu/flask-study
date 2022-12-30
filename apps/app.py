99c66c4b7ad8c4d39781b0f1998c304bf77bcdcf
umeyuu <umeyu7777777@gmail.com>
2022-12-30 14:18:00 +0900

    chapter2.4

--- apps/app.py
+++ apps/app.py
@@ -1,8 +1,27 @@
+from pathlib import Path
+
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+# SQLAlchemyをインスタンス化する
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
+    # アプリのコンフィグ設定
+    app.config.from_mapping(
+        SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
+        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        SQLALCHEMY=True,
+    )
+
+    # SQLAlchemyとアプリを連携する
+    db.init_app(app)
+    # Migrateとアプリを連携する
+    Migrate(app, db)
 
     from apps.crud import views as crud_views
 
--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template
 
+from apps.app import db
+from apps.crud.models import User
+
 # Blueprintでアプリを生成する
 crud = Blueprint("crud", __name__, template_folder="templates", static_folder="static")
 
@@ -7,3 +10,9 @@ crud = Blueprint("crud", __name__, template_folder="templates", static_folder="s
 @crud.route("/")
 def index():
     return render_template("crud/index.html")
+
+
+@crud.route("/sql")
+def sql():
+    db.session.query(User).all()
+    return "コンソールログを確認して下さい"
