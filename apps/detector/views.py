a9023626f990f8ae1865e965f0ef5242267a5b0d
umeyuu <umeyu7777777@gmail.com>
2022-12-30 23:24:01 +0900

    chapter07

--- apps/detector/views.py
+++ apps/detector/views.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
+import uuid
+from pathlib import Path
+
+from flask import (
+    Blueprint,
+    current_app,
+    redirect,
+    render_template,
+    send_from_directory,
+    url_for,
+)
+from flask_login import current_user, login_required
 
 from apps.app import db
 from apps.crud.models import User
+from apps.detector.forms import UploadImageForm
 from apps.detector.models import UserImage
 
 dt = Blueprint("detector", __name__, template_folder="templates")
@@ -18,3 +30,32 @@ def index():
     )
 
     return render_template("detector/index.html", user_images=user_images)
+
+
+@dt.route("/images/<path:filename>")
+def image_file(filename):
+    return send_from_directory(current_app.config["UPLOAD_FOLDER"], filename)
+
+
+@dt.route("/upload", methods=["GET", "POST"])
+# ログイン必須とする
+@login_required
+def upload_image():
+    form = UploadImageForm()
+    if form.validate_on_submit():
+        file = form.image.data
+        # ファイル名と拡張子を取得し、ファイル名をuuidに変換する
+        ext = Path(file.filename).suffix
+        image_uuid_file_name = str(uuid.uuid4) + ext
+
+        # 画像を保存する
+        image_path = Path(current_app.config["UPLOAD_FOLDER"], image_uuid_file_name)
+        file.save(image_path)
+
+        # DBに保存する
+        user_image = UserImage(user_id=current_user.id, image_path=image_uuid_file_name)
+        db.session.add(user_image)
+        db.session.commit()
+
+        return redirect(url_for("detector.index"))
+    return render_template("detector/upload.html", form=form)
