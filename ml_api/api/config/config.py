d142322fd9bd5862ab806d5c463345d8110bf327
umeyuu <umeyu7777777@gmail.com>
2023-01-02 01:32:38 +0900

    更新

--- ml_api/api/config/config.py
+++ ml_api/api/config/config.py
@@ -1,7 +1,12 @@
 import os
 from pathlib import Path
 
+basedir = Path(__file__).parent.parent
+
 
 class LocalConfig:
+    SQLALCHEMY_DATABASE_URI = "sqlite:////" + os.path.join(basedir, "images.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     INCLUDED_EXTENTION = [".png", ".jpg"]  # DBにいれる拡張子
     DIR_NAME = "handwriting_pics"  # 画像があるディレクトリ
